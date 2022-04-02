90fecdd257fa0e75984a10339f68452f210a1d33
Natalia Shvedova <lianess@yandex.ru>
2022-04-02 19:03:30 +0300

    task 1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -14,6 +14,11 @@ function addInCartMessage(title, price) {
     // добавьте в конец переменной message текст 'теперь в корзине!'
     // выведите переменную message в консоль
 
+
+    let message = productName;
+    message 'за' = productPrice 'теперь в корзине!';
+    console.log(message);
+
     // Конец решения задания №1.1.
 
     return message;
