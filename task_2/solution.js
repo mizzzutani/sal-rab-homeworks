704ca4921959ea9e9609896eaaecda195ac93178
Natalia Shvedova <lianess@yandex.ru>
2022-04-15 20:01:19 +0300

    solution task 2

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,19 +5,16 @@ function calcShipping(sum, min, shipping) {
 
     // Задание №2.1. Рассчитать доставку
 
-    // создайте переменную shippingSum
+    let shippingSum; 
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {
+        shippingSum = 0;
+      }  
 
-    // если productsSum равно 0,
-    // то shippingSum присвоить значение 0
+    if (productsSum > 0 && productsSum < freeShippingMinSum) {
+        shippingSum = shippingPrice;
+    }
 
-    // если productsSum Больше или равна freeShippingMinSum,
-    // то shippingSum присвоить значение 0
-
-    // если productsSum больше 0 и меньше freeShippingMinSum,
-    // то shippingSum присвоить значение shippingPrice
-
-    // Конец решения задания №2.1.
-
+     // Конец решения задания №2.1.
     return shippingSum;
 }
 
