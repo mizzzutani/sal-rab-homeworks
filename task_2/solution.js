c824c42f0bf75f2d3bbf368b6dc8cb37d8fc96f6
Natalia Shvedova <lianess@yandex.ru>
2022-04-15 21:27:09 +0300

    solution task 2

--- task_2/solution.js
+++ task_2/solution.js
@@ -47,7 +47,6 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // уменьшите totalSum на discountSum
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
-
     totalSum = (totalSum + shippingSum);
     let freeShipping;
     shippingSum == 0 ? (freeShipping = true) : (freeShipping = false); 
